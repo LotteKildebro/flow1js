96c46651c97cb82f2af17d52ee223f8bbe79e755
LotteKildebro <4514756@RTS-365.dk>
2020-09-09 09:38:53 +0200

    rettet i 28 og 28.1

--- script/opg28.js
+++ script/opg28.js
@@ -1,44 +1,42 @@
 //opgave 28 
 
-/*//lav array
-let knapper = ["knap 1", "knap 2", "knap 3", "knap 4", "knap 5"];
-//Kør igennem array med forEach
-knapper.forEach(knapFunktion, clickFunction);
+let ptags = document.querySelectorAll(".ptags");
+
+let nummer = 1;
+
+document.querySelectorAll(".ptags").forEach(ptags => {
+    ptags.innerHTML = ("Knap" + " " + nummer)
+    nummer += 1
+});
+
+document.querySelectorAll(".ptags").forEach(ptags => {
+    ptags.addEventListener("click", () => {
+        alert(ptags.innerHTML)
+    })
+});
+
+28.1
+
+let divs = document.querySelectorAll(".div");
+console.log(divs);
+
+document.querySelectorAll(".div").forEach(divs => {
+    switch (divs.innerHTML) {
+        case "Rip":
+            break;
+        case "Rap":
+            divs.style.color = "blue";
+            break;
+        case "Rup":
+            divs.addEventListener("click", function () {
+                alert("Rup")
+            })
+            break;
+    }
+});
 
 
-function knapFunktion(knap) {
-    document.querySelector(".ptags").innerHTML +=
-        knap + "<br>";
-    console.log(knap);
-};
 
-document.querySelector(".ptags").addEventListener("click", clickFunction);
-
-function clickFunction(knap) {
-    document.querySelector(".ptags").innerHTML += knapper + "<br>";
-}*/
-
-//Det er lige gået op for mig at jeg har læst opg 28 forkert.. vender tilbage til opgaven hvis
-//jeg kan nå det ;-)
-
-let AlleDivs = document.querySelectorAll(".div");
-console.log(AlleDivs);
-
-AlleDivs.forEach(function (div) {
-    console.log(div);
-})
-
-//rup alert
-document.querySelector(".rup").addEventListener("click", clickFunction);
-
-function clickFunction() {
-    alert("rup")
-};
-
-//rap styling
-let sheet = document.createElement('style')
-sheet.innerHTML = ".rap {font-size: 2em; color: #34bbdd;}";
-document.body.appendChild(sheet);
 
 //opgave 28.2
 
